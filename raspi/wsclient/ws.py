241242de6692e0e498e34edb85cf40bcd8dd0585
chekke1999 <nevec555@gmail.com>
2020-10-20 11:11:21 +0900

    引数名変更uri -> url

--- raspi/wsclient/ws.py
+++ raspi/wsclient/ws.py
@@ -2,11 +2,11 @@
 import asyncio,json,websockets
 class ws:
     @staticmethod
-    async def _send(uri,jlist):
-        async with websockets.connect(uri) as websocket:
+    async def _send(url,jlist):
+        async with websockets.connect(url) as websocket:
             await websocket.send(json.dumps(jlist))
             recv = await websocket.recv()
             print(json.loads(recv))
     @classmethod
-    def send(cls,uri,jlist):
-        asyncio.get_event_loop().run_until_complete(cls._send(uri,jlist))
+    def send(cls,url,jlist):
+        asyncio.get_event_loop().run_until_complete(cls._send(url,jlist))
