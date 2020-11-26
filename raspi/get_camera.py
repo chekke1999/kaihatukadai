e321b0d704ebec114d0ab727844e60ebd50bcdc5
irasa1121 <ooqant21@gmail.com>
2020-11-26 16:04:33 +0900

    banana

--- raspi/get_camera.py
+++ raspi/get_camera.py
@@ -5,24 +5,16 @@ from imutils.video import FPS
 import imutils,cv2
 import numpy as np
 
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-n", "--num-frames", type=int, default=100,
-#	help="# of frames to loop over for FPS test")
-#ap.add_argument("-d", "--display", type=int, default=-1,
-#	help="Whether or not frames should be displayed")
-#args = vars(ap.parse_args())
-
-print("[INFO] sampling THREADED frames from webcam...")
-vs = WebcamVideoStream(src=0).start()
-fps = FPS().start()
-
-WIDTH = 1920
-#HEIGHT = 1080
-
 def img_get(pipe):
+	print("[INFO] sampling THREADED frames from webcam...")
+	vs = WebcamVideoStream(src=0).start()
+	fps = FPS().start()
+
+	WIDTH = 1000
+
 	while(True):
 		frame = vs.read()
-		pipe.send(frame)
+		pipe = [frame]
 		frame = imutils.resize(frame, width=WIDTH)
 		cv2.imshow("Frame", frame)
 		key = cv2.waitKey(1) & 0xFF
@@ -35,9 +27,5 @@ def img_get(pipe):
 	fps.stop()
 
 if __name__ == '__main__':
-	print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-	print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-	print("[INFO] width: {:.0f}".format(WIDTH))
-	print(type(frame))
 	cv2.destroyAllWindows()
 	vs.stop()
