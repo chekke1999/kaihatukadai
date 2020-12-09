206b4695563f99acd5ca8be2d063de29f83964d3
chekke1999 <nevec555@gmail.com>
2020-12-09 16:23:47 +0900

    update

---
+++ server/DB/sql/testdatainsert.sql
@@ -0,0 +1,37 @@
+use piscan
+INSERT INTO pi_camera(
+    plc_mac,
+    top_img_path,
+    slanting_img_path,
+    scan_data
+) VALUES( 
+    '58:52:8a:d6:69:a1',
+    '/root/picture/',
+    '/root/picture/',
+    '{"status":{"temp":33,"hr":50,"atm":1013.25,"luminance":20000},"type":"A","parts":{"R1":{"mounted_parts":true,"misalignment":false},"R2":{"mounted_parts":false,"misalignment":true},"R3":{"mounted_parts":true,"misalignment":true},"IC":{"mounted_parts":true,"misalignment":true,"angle":0.1}}}'
+);
+
+INSERT INTO pi_camera(
+    plc_mac,
+    top_img_path,
+    slanting_img_path,
+    scan_data
+) VALUES( 
+    '58:52:8a:d6:69:a1',
+    '/root/picture/',
+    '/root/picture/',
+    '{"status":{"temp":33,"hr":50,"atm":1013.25,"luminance":20000},"type":"B","parts":{"R1":{"mounted_parts":true,"misalignment":true},"R2":{"mounted_parts":true,"misalignment":true},"R3":{"mounted_parts":true,"misalignment":true},"IC":{"mounted_parts":true,"misalignment":true,"angle":0.1}}}'
+);
+
+INSERT INTO pi_camera(
+    plc_mac,
+    top_img_path,
+    slanting_img_path,
+    scan_data
+) VALUES( 
+    '58:52:8a:d6:69:a1',
+    '/root/picture/',
+    '/root/picture/',
+    '{"status":{"temp":33,"hr":50,"atm":1013.25,"luminance":20000},"type":"C","parts":{"R1":{"mounted_parts":true,"misalignment":false},"R2":{"mounted_parts":false,"misalignment":true},"R3":{"mounted_parts":true,"misalignment":true},"IC":{"mounted_parts":true,"misalignment":true,"angle":0.1}}}'
+);
+go
